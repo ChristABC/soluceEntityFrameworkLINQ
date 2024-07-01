4a2565c976c3a2cdb593312a9348d8b34fa18de8
Christ ASSANA <christassana@gmail.com>
2024-07-01 12:30:06 +0200

    Ajoutez des fichiers projet.

---
+++ projEntityFrameworkLINQ/Model.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace projEntityFrameworkLINQ
+{
+    internal class Model
+    {
+        public class AnimalContext : DbContext
+        {
+            public DbSet<Animal> Animals { get; set; }
+            public DbSet<Species> Species { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder builder)
+            {
+                builder.UseSqlServer("Your connection string");
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                // Seed initial des espèces protégées avec le nombre d'animaux recensés
+                modelBuilder.Entity<Species>().HasData(
+                    new Species { SpeciesId = 1, Name = "Cougars blancs", RemainingCount = 3 },
+                    new Species { SpeciesId = 2, Name = "Tigres blancs", RemainingCount = 100 },
+                    new Species { SpeciesId = 3, Name = "Tortues albinos", RemainingCount = 15 }
+                );
+            }
+        }
+
+        public class Animal
+        {
+            public String Name { get; set; }
+
+            public Species Species { get; set; }
+        }
+
+        public class Species
+        {
+            public int SpeciesId { get; set; }
+            public String Name { get; set; }
+            public int RemainingCount { get; set; }
+        }
+
+    }
+}
---
+++ projEntityFrameworkLINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static projEntityFrameworkLINQ.Model;
+using System.Collections.Generic;
+
+namespace LinqMethod
+{
+
+    public class MainProgram
+    {
+        public static int RemainingCount { get; private set; }
+
+        public static void Main(string[] args)
+        {
+
+            using (var context = new AnimalContext())
+            {
+                var whiteCougarsCount = context.Animals.Count(animal => animal.Species.Name == "Cougars blancs");
+                var whiteTigersCount = context.Animals.Count(animal => animal.Species.Name == "Tigres blancs");
+                var albinoTurtlesCount = context.Animals.Count(animal => animal.Species.Name == "Tortues albinos");
+
+                Console.WriteLine("Nombre d'animaux recensés :");
+                Console.WriteLine($"Cougars blancs : {whiteCougarsCount}");
+                Console.WriteLine($"Tigres blancs : {whiteTigersCount}");
+                Console.WriteLine($"Tortues albinos : {albinoTurtlesCount}");
+            }
+        }
+    }
+}
